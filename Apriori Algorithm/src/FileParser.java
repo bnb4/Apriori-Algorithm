e3a6cf05eef4764079cd49d3e2eada26fae2ca23
RainyRuRu <believeu123@gmail.com>
2016-05-25 01:49:24 +0800

    gui and fileParser

--- Apriori Algorithm/src/FileParser.java
+++ Apriori Algorithm/src/FileParser.java
@@ -1,5 +1,11 @@
+import java.util.List;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -8,70 +14,135 @@ import java.util.Scanner;
 public class FileParser {
 	
 	// 儲存資料
-	private AttributeData [] attributeDatas;
+	private static Map<String, String[]> attibutesMap = new HashMap<String, String[]>();
+	private static List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+	private static List<String> attribute = new ArrayList<String>();
+
+	public static String PATH = "";
 	
-	/**
-	 * 建構子
-	 * @param path 檔案路徑
-	 */
-	public FileParser(String path) {
+	private static boolean readFile() {
+		
+		if (PATH.equals("")){
+			return false;
+		}
 		
 		boolean isReadAttribute = false;
-		try (Scanner scanner = new Scanner(new File(path))) {
+		try (Scanner scanner = new Scanner(new File(PATH))) {
 			// 讀取第一行(屬性名稱)
-			while (!isReadAttribute && scanner.hasNext()) {
-				isReadAttribute = loadAttribute(scanner.nextLine());
-			}
-			// 讀取資料行
+			
 			while (scanner.hasNext()) {
-				addData(scanner.nextLine());
+				String line = scanner.nextLine().trim();
+
+				if (line.equals("﻿@Attribute")) {
+					isReadAttribute = true;
+					continue;
+				}
+				if (line.equals("@Data")) {
+					isReadAttribute = false;
+					continue;
+				}
+				if (isReadAttribute) {
+					loadAttribute(line);
+				}
+				
+				if (!isReadAttribute) {
+					addData(line);
+				}
 			}
+			
+			return true;
+			
 		} catch (IOException e) { 
 			e.printStackTrace(); 
+			return false;
+		} catch (Exception e) {
+			return false;
 		}
 	}
 	
 	/**
-	 * 載入屬性名稱 (需要放在文件讀取第一行)
-	 * @param data 讀取資料
-	 * @return 是否成功讀取
+	 * 載入屬性名稱與定義
+	 * @param line 讀取資料
 	 */
-	private boolean loadAttribute(String data) {
+	private static void loadAttribute(String line) {
+		
 		// 跳過空行
-		if (data.trim().length() == 0 ) return false;
+		if (line.trim().length() == 0 ) {
+			return;
+		}
 		
-		// 處理屬性名稱
-		String [] raw = data.trim().split(" ");
+		// 處理屬性名稱與定義
+		String[] att = line.split(":");
+		String[] values = att[1].trim().split("\\s*,\\s*");
 		
-		// 創建儲存物件
-		attributeDatas = new AttributeData [raw.length];
-		for (int i = 0; i < raw.length; i++) 
-			attributeDatas[i] = new AttributeData(raw[i]);
-		return true;
+		attibutesMap.put(att[0].trim(), values);
+		attribute.add(att[0].trim());
 	}
 	
 	/**
-	 * 載入一筆資料 (資料只能為 True False True...)
-	 * @param data 資料
+	 * 載入一筆資料
+	 * @param line 資料
 	 */
-	public void addData(String data) {
+	public static void addData(String line) {
+		
 		// 跳過空行
-		if (data.trim().length() == 0 ) return;
-
+		if (line.trim().length() == 0 ) {
+			return;
+		}
+		
 		// 分割資料
-		String[] raw = data.trim().split(" ");
+		String[] values = line.trim().split("\\s*\t\\s*");
+		Map<String, String> map = new HashMap<>();
+		
+		int index = 0;
+		//比對資料是否符合定義
+		for (String key : attribute) {
+			boolean isExist = Arrays.asList(attibutesMap.get(key)).contains(values[index]);
+			if (isExist) {
+				map.put(key, values[index]);
+			}
+			index++;
+		}
 		
 		// 存入資料
-		for (int i = 0; i < raw.length; i++) 
-			attributeDatas[i].addData(Boolean.parseBoolean(raw[i]));
+		if (map.size() == attibutesMap.keySet().size()){
+			data.add(map);
+		}
+
 	}
 	
 	/**
-	 * 取得目前所有資料
+	 * 取得屬性定義
+	 * @return 格式過的屬性資料
+	 */
+	public static Map<String, String[]> getAttributeInfo() {
+		return attibutesMap;
+	}
+	
+	/**
+	 * 取得屬性定義
+	 * @return 格式過的屬性資料
+	 */
+	public static List<String> getAttributes() {
+		return attribute;
+	}
+	
+	
+	/**
+	 * 取得所有資料
 	 * @return 格式過的資料
 	 */
-	public AttributeData [] getAllData() {
-		return attributeDatas;
+	public static List<Map<String, String>> getAllData() {
+		return data;
+	}
+	
+	/**
+	 * 設定檔案路徑
+	 * @param 路徑
+	 */
+	public static boolean setPath(String path) {
+		PATH = path;
+		return readFile();
 	}
 	
 }
--- Apriori Algorithm/src/Main.java
+++ Apriori Algorithm/src/Main.java
@@ -7,64 +7,6 @@ public class Main {
 		
 		Gui gui = new Gui();
 		
-		// 這是使用的DEMO
-		FileParser fileParser = new FileParser("demo1.txt");
-		AttributeData [] attributeDatas = fileParser.getAllData();
-		AprioriCoverageCalc aprioriCoverageCalc = new AprioriCoverageCalc(attributeDatas);
-		
-		// 定義涵蓋率
-		double minCoverage = 0.4;
-		
-		// 取得 Single-Set
-		Map<String, Double> singleSetItems = new HashMap<>();
-		
-		// 初始化 Single-Set
-		for (AttributeData ad : attributeDatas) {
-			singleSetItems.put(ad.getAttributeName() + ":True", 0.0);
-			singleSetItems.put(ad.getAttributeName() + ":False", 0.0);
-		}
-		
-		// 檢查若有低於涵蓋率的，剃除
-		for (String filter : singleSetItems.keySet().toArray(new String[singleSetItems.size()])) {
-			double coverage = aprioriCoverageCalc.getCoverage(filter);
-			if (coverage < minCoverage) singleSetItems.remove(filter);
-			else singleSetItems.put(filter, coverage);
-		}
-		
-		// 印出符合的 Single-Set Items
-		System.out.println("Single-Set Items:");
-		for (String item : singleSetItems.keySet()) {
-			String [] splItem = item.split(":", 2);
-			System.out.format("%s = %s  Coverage: %.01f\r\n", splItem[0], splItem[1], singleSetItems.get(item));
-		}
-		System.out.println("\r\n");
-		
-		// 取得 Double-Set
-		Map<String, Double> doubleSetItems = new HashMap<>();
-		
-		// 初始化 Double-Set
-		String [] singleAttributes = singleSetItems.keySet().toArray(new String[singleSetItems.size()]);
-		for (int i = 0; i < singleAttributes.length; i++) 
-			for (int j = i + 1; j < singleAttributes.length; j++) 
-				if (!singleAttributes[i].equals(singleAttributes[j]))
-					doubleSetItems.put(singleAttributes[i] + "&" + singleAttributes[j], 0.0);
-
-		// 檢查若有低於涵蓋率的，剃除
-		for (String filter : doubleSetItems.keySet().toArray(new String[doubleSetItems.size()])) {
-			double coverage = aprioriCoverageCalc.getCoverage(filter);
-			if (coverage < minCoverage) doubleSetItems.remove(filter);
-			else doubleSetItems.put(filter, coverage);
-		}		
-		
-		// 印出符合的 Double-Set Items
-		System.out.println("Double-Set Items:");
-		for (String item : doubleSetItems.keySet()) {
-			String [] splItem = item.split("&", 2);
-			String [] splItem1 = splItem[0].split(":", 2);
-			String [] splItem2 = splItem[1].split(":", 2);
-			System.out.format("%s = %s & %s = %s Coverage: %.01f\r\n", splItem1[0], splItem1[1], splItem2[0], splItem2[1], doubleSetItems.get(item));
-		}
-		System.out.println("\r\n");
 	}
 
 }
